69dff3898c8c1a009ca50572e5d049cbf57d46c8
Lars Synslien Kvihaugen <lars.synslien.kvihaugen@gmail.com>
2023-05-10 15:12:25 +0200

    Create loaddata.js

---
+++ loaddata.js
@@ -0,0 +1,23 @@
+loadData();
+async function loadData(){
+    const response = await fetch("https://raw.githubusercontent.com/LarsSK06/LarsSK06/main/mydata.json");
+    const data = await response.json();
+    if(document.getElementById("socials")){
+        const cache = document.getElementById("socials");
+        for(let i = 0; i < data.socials.length; i++){
+            cache.innerHTML += `<a href="${data.socials[i].href}">${data.socials[i].name}</a><br>\n`;
+        }
+    }
+    if(document.getElementById("contact")){
+        const cache = document.getElementById("contact");
+        for(let i = 0; i < data.contact.length; i++){
+            cache.innerHTML += `${data.contact[i]}\n`;
+        }
+    }
+    if(document.getElementById("credentials")){
+        const cache = document.getElementById("credentials");
+        for(let i = 0; i < data.credentials.length; i++){
+            cache.innerHTML += `${data.credentials[i]}\n`;
+        }
+    }
+}
